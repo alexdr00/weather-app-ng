9fee769f560b97e7e9c941eb0cae1924f459826a
Alexander Dominguez <alexrendon2109@gmail.com>
2019-07-07 18:30:54 -0500

    added some components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../lib/material';
+import { LoginComponent } from './login/login.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchHistoryComponent } from './search-history/search-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavigationBarComponent,
+    RegisterComponent,
+    SearchHistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]

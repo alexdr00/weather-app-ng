a045a6f2d9bee095279ec4ff83d96198407c7be8
Alexander Dominguez <alexander.dominguez@jayacompany.com>
2019-07-11 11:44:02 -0500

    added a simple directive

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import {HighlightModule} from '../directives/highlight';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    HighlightModule
   ]
 })
 export class HomeModule { }

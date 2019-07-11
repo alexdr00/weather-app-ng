29992ab46a89a188b98a57a86300b60132b78d47
Alexander Dominguez <alexander.dominguez@jayacompany.com>
2019-07-11 17:00:43 -0500

    added animations with angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('move', [
+      state('top', style({
+        top: '12%'
+      })),
+
+      state('middle', style({
+        top: '50%'
+      })),
+
+      transition('middle => top', [
+        animate('1s')
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
